7e44a29d8da4c718a6cd0570fd5d4767ffaf8486
MinhTuan3012 <tuanhm3012@gmail.com>
2016-07-01 00:13:49 +0700

    Homework

---
+++ ProjectAtom/scripts/Tank.js
@@ -0,0 +1,49 @@
+class Tank{
+  constructor(x, y, group){
+    this.sprite = group.create(x, y, 'tankDown');
+    //tuong tac vat ly
+    TankOnline.game.physics.arcade.enable(this.sprite);
+    this.sprite.anchor.set(0.5,0.5);
+    this.direction = new Phaser.Point(0,1);
+    this.lastShorTime = TankOnline.game.time.now;
+    this.sprite.body.collideWorldBounds = true;
+    this.sprite.health = 5;
+  }
+
+  update(direction,direction1){
+    if(direction.x < 0){
+      this.sprite.body.velocity.x = -250;
+      this.sprite.loadTexture('tankLeft');
+      this.direction = new Phaser.Point(-1,0);
+    }
+    else if (direction.x > 0){
+      this.sprite.body.velocity.x = 250;
+      this.sprite.loadTexture('tankRight');
+      this.direction = new Phaser.Point(1,0);
+    }
+    else{
+      this.sprite.body.velocity.x = 0;
+    }
+
+    if(direction.y < 0){
+      this.sprite.body.velocity.y = -250;
+      this.sprite.loadTexture('tankUp');
+      this.direction = new Phaser.Point(0,-1);
+    }
+    else if (direction.y > 0){
+      this.sprite.body.velocity.y = 250;
+      this.sprite.loadTexture('tankDown');
+      this.direction = new Phaser.Point(0,1);
+    }
+    else{
+      this.sprite.body.velocity.y = 0;
+    }
+  }
+
+  fire(){
+    if(TankOnline.game.time.now - this.lastShorTime > 200){
+      this.lastShorTime = TankOnline.game.time.now;
+      new Bullet(this);
+    }
+  }
+}
