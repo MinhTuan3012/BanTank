3768df8b2ae4707780ea8a567476f0efb66a5f91
MinhTuan3012 <tuanhm3012@gmail.com>
2016-07-02 21:10:04 +0700

    Online

--- ProjectAtom/scripts/Tank.js
+++ ProjectAtom/scripts/Tank.js
@@ -1,16 +1,15 @@
 class Tank{
   constructor(x, y, group){
     this.sprite = group.create(x, y, 'tankDown');
-    //tuong tac vat ly
     TankOnline.game.physics.arcade.enable(this.sprite);
     this.sprite.anchor.set(0.5,0.5);
     this.direction = new Phaser.Point(0,1);
-    this.lastShorTime = TankOnline.game.time.now;
+    this.lastShotTime = TankOnline.game.time.now;
     this.sprite.body.collideWorldBounds = true;
     this.sprite.health = 5;
   }
 
-  update(direction,direction1){
+  update(direction){
     if(direction.x < 0){
       this.sprite.body.velocity.x = -250;
       this.sprite.loadTexture('tankLeft');
@@ -37,13 +36,6 @@ class Tank{
     }
     else{
       this.sprite.body.velocity.y = 0;
-    }
-  }
-
-  fire(){
-    if(TankOnline.game.time.now - this.lastShorTime > 200){
-      this.lastShorTime = TankOnline.game.time.now;
-      new Bullet(this);
     }
   }
 }
